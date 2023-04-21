ca0ef2f4a3007f53bfddd62744a254316db8e79b
Afeez Aziz <afeezaziz@gmail.com>
2023-04-21 17:52:39 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 require __DIR__.'/web/application.php';
 require __DIR__.'/web/credit.php';
